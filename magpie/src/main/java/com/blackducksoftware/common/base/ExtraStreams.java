29c78806c210a496a9c48dc71848435889c27d2c
Jeremy Gustie <jgustie@blackducksoftware.com>
2017-02-08 10:39:06 -0500

    Add ExtraStreams for stream helpers

---
+++ magpie/src/main/java/com/blackducksoftware/common/base/ExtraStreams.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Black Duck Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.blackducksoftware.common.base;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * Extra stream helpers.
+ *
+ * @author jgustie
+ */
+public final class ExtraStreams {
+
+    /**
+     * A function for filtering a stream by type. For example:
+     *
+     * <pre>
+     * List<String> strs = objs.stream().flatMap(instanceOf(String.class)).collect(toList());
+     * </pre>
+     *
+     * Is another way of doing:
+     *
+     * <pre>
+     * List<String> strs = new ArrayList<>();
+     * for (Object obj : objs) {
+     *     if (obj instanceof String) {
+     *         strs.add((String) obj);
+     *     }
+     * }
+     * </pre>
+     */
+    public static <T> Function<Object, Stream<T>> instanceOf(Class<T> type) {
+        Objects.requireNonNull(type);
+        return obj -> type.isInstance(obj) ? Stream.of(type.cast(obj)) : Stream.empty();
+    }
+
+    private ExtraStreams() {
+        assert false;
+    }
+}
