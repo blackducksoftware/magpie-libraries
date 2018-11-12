c49276954bbd369e3029c001aa6ba5cec2fd200b
Jeremy Gustie <jgustie@blackducksoftware.com>
2018-11-12 17:50:10 -0500

    Add ExtraConsumers.withBoth to invoke a consumer repeatedly

---
+++ magpie/src/main/java/com/blackducksoftware/common/base/ExtraConsumers.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Synopsys, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.blackducksoftware.common.base;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * Extra consumer helpers.
+ *
+ * @author jgustie
+ */
+public class ExtraConsumers {
+
+    /**
+     * Returns a {@link BiConsumer} that invokes the supplied consumer with both arguments (in argument order).
+     */
+    public static <T> BiConsumer<T, T> withBoth(Consumer<? super T> c) {
+        return (a, b) -> {
+            c.accept(a);
+            c.accept(b);
+        };
+    }
+
+}
